1454f6a618f582eaa28d928fde6ca2b8b592200f
贾艳超 <jiayanchao18031324420@gmail.com>
2019-05-21 20:28:04 +0800

    增加侧边栏

---
+++ src/components/SideBar.vue
@@ -0,0 +1,199 @@
+<template>
+  <div id="side-bar" class="container">
+    <div id="hot-topic" class="side-bar">
+      <div class="bar-head">
+        <a href="https://gocn.vip/topic/channel-hot" class="pull-right">更多</a>
+        <h5>热门话题</h5>
+      </div>
+      <div class="bar-body">
+        <dl>  
+            <!-- dl大框   
+            内部先是dt图片  
+            dd 内部 p标(包span a) + p关系数字(再包b) 
+            再是 
+             -->
+          <dt class="pull-left aw-border-radius-5">
+            <a href="https://gocn.vip/topic/每日新闻">
+              <img alt src="https://gocn.vip/static/common/topic-mid-img.png">
+            </a>
+          </dt>
+          <dd class="pull-left">
+            <p class="clearfix">
+              <span class="topic-tag">
+                <a href="https://gocn.vip/topic/每日新闻" class="text" data-id="49">每日新闻</a>
+              </span>
+            </p>
+            <p>
+              <b>330</b> 个问题,
+              <b>362</b> 人关注
+            </p>
+          </dd>
+        </dl>
+        <dl>
+          <dt class="pull-left aw-border-radius-5">
+            <a href="https://gocn.vip/topic/golang">
+              <img
+                alt
+                src="https://gocn.vip/uploads/topic/20161010/42f5d67729cb6350aab381149a665e85_50_50.jpg"
+              >
+            </a>
+          </dt>
+          <dd class="pull-left">
+            <p class="clearfix">
+              <span class="topic-tag">
+                <a href="https://gocn.vip/topic/golang" class="text" data-id="12">golang</a>
+              </span>
+            </p>
+            <p>
+              <b>505</b> 个问题,
+              <b>634</b> 人关注
+            </p>
+          </dd>
+        </dl>
+        <dl>
+          <dt class="pull-left aw-border-radius-5">
+            <a href="https://gocn.vip/topic/新手入门">
+              <img alt src="https://gocn.vip/static/common/topic-mid-img.png">
+            </a>
+          </dt>
+          <dd class="pull-left">
+            <p class="clearfix">
+              <span class="topic-tag">
+                <a href="https://gocn.vip/topic/新手入门" class="text" data-id="21">新手入门</a>
+              </span>
+            </p>
+            <p>
+              <b>8</b> 个问题,
+              <b>197</b> 人关注
+            </p>
+          </dd>
+        </dl>
+        <dl>
+          <dt class="pull-left aw-border-radius-5">
+            <a href="https://gocn.vip/topic/区块链">
+              <img alt src="https://gocn.vip/static/common/topic-mid-img.png">
+            </a>
+          </dt>
+          <dd class="pull-left">
+            <p class="clearfix">
+              <span class="topic-tag">
+                <a href="https://gocn.vip/topic/区块链" class="text" data-id="39">区块链</a>
+              </span>
+            </p>
+            <p>
+              <b>27</b> 个问题,
+              <b>241</b> 人关注
+            </p>
+          </dd>
+        </dl>
+      </div>
+    </div>
+
+    <div id="hot-user" class="side-bar">
+      <div class="bar-head"></div>
+      <a href="https://gocn.vip/topic/channel-hot" class="pull-right">更多</a>
+      <h5>热门用户</h5>
+      <div class="bar-body">
+        <dl>
+          <dt class="pull-left aw-border-radius-5">
+            <a href="https://gocn.vip/people/Xargin">
+              <img alt src="https://gocn.vip/uploads/avatar/000/00/01/59_avatar_mid.jpg">
+            </a>
+          </dt>
+          <dd class="pull-left">
+            <a href="https://gocn.vip/people/Xargin" data-id="159" class="aw-user-name">Xargin</a>
+            <p class="signature"></p>
+            <p>
+              <b>100</b> 个问题,
+              <b>73</b> 次赞同
+            </p>
+          </dd>
+        </dl>
+
+        <dl>
+          <dt class="pull-left aw-border-radius-5">
+            <a href="https://gocn.vip/people/huhuyou2">
+              <img alt src="https://gocn.vip/static/common/avatar-mid-img.png">
+            </a>
+          </dt>
+          <dd class="pull-left">
+            <a href="https://gocn.vip/people/huhuyou2" data-id="2317" class="aw-user-name">huhuyou2</a>
+            <p class="signature"></p>
+            <p>
+              <b>19</b> 个问题,
+              <b>4</b> 次赞同
+            </p>
+          </dd>
+        </dl>
+
+        <dl>
+          <dt class="pull-left aw-border-radius-5">
+            <a href="https://gocn.vip/people/chenqinghe">
+              <img alt src="https://gocn.vip/uploads/avatar/000/00/04/23_avatar_mid.jpg">
+            </a>
+          </dt>
+          <dd class="pull-left">
+            <a
+              href="https://gocn.vip/people/chenqinghe"
+              data-id="423"
+              class="aw-user-name"
+            >chenqinghe</a>
+            <p class="signature"></p>
+            <p>
+              <b>24</b> 个问题,
+              <b>20</b> 次赞同
+            </p>
+          </dd>
+        </dl>
+
+        <dl>
+          <dt class="pull-left aw-border-radius-5">
+            <a href="https://gocn.vip/people/heramerom">
+              <img alt src="https://gocn.vip/static/common/avatar-mid-img.png">
+            </a>
+          </dt>
+          <dd class="pull-left">
+            <a
+              href="https://gocn.vip/people/heramerom"
+              data-id="1721"
+              class="aw-user-name"
+            >heramerom</a>
+            <p class="signature"></p>
+            <p>
+              <b>25</b> 个问题,
+              <b>6</b> 次赞同
+            </p>
+          </dd>
+        </dl>
+
+        <dl>
+          <dt class="pull-left aw-border-radius-5">
+            <a href="https://gocn.vip/people/Ky">
+              <img alt src="https://gocn.vip/uploads/avatar/000/00/08/35_avatar_mid.jpg">
+            </a>
+          </dt>
+          <dd class="pull-left">
+            <a href="https://gocn.vip/people/Ky" data-id="835" class="aw-user-name">Ky</a>
+            <p class="signature"></p>
+            <p>
+              <b>21</b> 个问题,
+              <b>10</b> 次赞同
+            </p>
+          </dd>
+        </dl>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "side-bar"
+};
+</script>
+
+<style scoped>
+.side-bar {
+  text-align: justify;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,47 @@
 <!--模板-->
 <template>
-    <div id="home">
-
-        <!--        整理首页内容
+  <div id="home">
+    <!--        整理首页内容
             1 导航栏    nva        原来叫top-menu
             2 高层菜单  category-menu
             3 发现页    find-page
             4 侧边栏  热门用户和热门文章 hot-topics
 
             5 页脚
-        -->
-        <forum-header></forum-header>
-        <container-wrap></container-wrap>
-        <common-list></common-list>
+    -->
+    <forum-header></forum-header>
 
-    </div>
-
-
-
-
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="8">
+          <container-wrap></container-wrap>
+          <common-list></common-list>
+        </b-col>
+        <b-col sm="4">
+            <side-bar> </side-bar>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <!--行为-->
 <script>
+import ForumHeader from "../components/ForumHeader";
+import ContainerWrap from "../components/ContainerWrap";
+import CommonList from "../components/CommonList";
+import SideBar from "../components/SideBar";
 
-    import ForumHeader from '../components/ForumHeader'
-    import ContainerWrap from '../components/ContainerWrap'
-    import CommonList from '../components/CommonList'
-
-    export default {
-        name: 'home',
-        components: {
-            ForumHeader,
-            ContainerWrap,
-            CommonList,
-
-        }
-    }
+export default {
+  name: "home",
+  components: {
+    ForumHeader,
+    ContainerWrap,
+    CommonList,
+    SideBar
+  }
+};
 </script>
 
 <style>
-
-
-
 </style>
